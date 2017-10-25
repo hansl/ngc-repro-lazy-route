8785054ad7c4711dad7b23825205a2ad9e25c463
Angular CLI E2e <angular-core+e2e@google.com>
2017-10-25 15:15:44 -0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+
+      import { RouterModule } from '@angular/router';
+      import { ReactiveFormsModule } from '@angular/forms';
+    import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import * as lazy_module_1 from './lazy/lazy.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,6 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+      RouterModule.forRoot([{ path: "lazy", loadChildren: "./lazy/lazy.module#LazyModule" }]),
+      // lazy_module_1.LazyModule,
+      ReactiveFormsModule,
+
     BrowserModule
   ],
   providers: [],
